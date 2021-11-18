db0130c7a7f2f434f5f59c5e64a6f29e1b75815a
joaobatistajrs <joao.batista.jrs@gmail.comgit config --global user.email joao.batista.jrs@gmail.comgit config --global user.email joao.batista.jrs@gmail.com>
2021-11-18 20:51:28 -0300

    atualiza Js

--- Planos/script.js
+++ Planos/script.js
@@ -1,3 +1,8 @@
 function ConfirmaMensagem(){
     alert('Obrigado por sua compra.');
 }
+
+
+function abrirPopUp(){
+    janela = window.open('/Planos/confia.html', 'nova_janela', 'width=500, height=500' )
+   }
